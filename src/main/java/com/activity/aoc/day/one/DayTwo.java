67f1af1db58c7599ff46317a80f03fafc10116ab
phdincoding <aashish.panvelkar@gmail.com>
2024-05-30 07:38:50 -0400

    changes

--- src/main/java/com/activity/aoc/day/one/DayTwo.java
+++ src/main/java/com/activity/aoc/day/one/DayTwo.java
@@ -20,15 +20,16 @@ public class DayTwo {
 
 
         HashMap<String, Integer> limitColor = new HashMap<>();
-        limitColor.put("red",12);
-        limitColor.put("blue",14);
-        limitColor.put("green",13);
+        limitColor.put("red", 12);
+        limitColor.put("blue", 14);
+        limitColor.put("green", 13);
 
 
         BufferedReader reader = null;
         try {
             File file = new File("inputs/daythree.txt");
             reader = new BufferedReader(new FileReader(file));
+            BigDecimal finalNumber = BigDecimal.ZERO;
 
             String line;
 
@@ -41,11 +42,14 @@ public class DayTwo {
                 System.out.println(line);
                 String[] temp = line.split(":");
                 String gameNum = temp[0]; // Gives which game it is
+                System.out.println("Starting " + gameNum);
                 for (String game : temp) {
 
                     String colorRound = temp[1];
                     String[] round = colorRound.split(";");
+                    boolean thresholdReached = false;
                     for (String turn : round) {
+                        System.out.println("\nNext round starting\n");
                         String[] color = turn.split(",");
                         for (String eachColor : color) {
 
@@ -53,33 +57,46 @@ public class DayTwo {
                             String[] colorAndCount = eachColor.split(" ");
                             String colorName = colorAndCount[2];
                             String colorCount = colorAndCount[1];
+
                             System.out.println("Color: " + colorName + " Count: " + eachColorCount.get(colorName));
                             colorCounter += eachColorCount.get(colorName) + Integer.parseInt(colorCount);
-
-                            if (eachColorCount.get(colorName).compareTo(limitColor.get(colorName))>0){
-                                System.out.println("Limit Reached");
+                            if (eachColorCount.get(colorName).compareTo(limitColor.get(colorName)) > 0) {
+                                System.out.println("****Disqualified****");
                                 System.out.println("Color: " + colorName + " Count: " + eachColorCount.get(colorName));
                                 System.out.println("Limit: " + limitColor.get(colorName));
-                                break;
-                            }else{
-
+                                thresholdReached = true;
+                            }
+                            if (!thresholdReached) {
+                                System.out.println("Adding " + colorName + " Counter: " + colorCounter);
                                 eachColorCount.put(colorName, colorCounter);
                             }
 
+
+                        }
+                        if (!thresholdReached) {
+                            System.out.println("Final Number Added because threshold not reached in this game");
+                            String[] num = gameNum.split(" ");
+                            finalNumber = finalNumber.add(new BigDecimal(num[1]));
+                            System.out.println("Game: " + num[1] + " added");
+
                         }
 
+
                     }
-                }
-                for (Map.Entry<String, Integer> entry : eachColorCount.entrySet()) {
-
-                    String key = entry.getKey();
-                    System.out.println("Color: " + key);
-                    int value = entry.getValue();
-                    System.out.println("Value: " + value);
 
 
                 }
+//                for (Map.Entry<String, Integer> entry : eachColorCount.entrySet()) {
+//
+//                    String key = entry.getKey();
+//                    System.out.println("Color: " + key);
+//                    int value = entry.getValue();
+//                    System.out.println("Value: " + value);
+//
+//
+//                }
             }
+            System.out.println("Number of games not disqualified: " + finalNumber);
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
